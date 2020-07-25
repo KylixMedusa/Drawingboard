c2e4f2add03fdef0dfcb124bb062e42d46758077
Aayush Agarwal <ayush.agarwalmng@gmail.com>
2020-07-25 22:26:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -62,3 +62,45 @@ let isDrawing = false;
         colorpicker.addEventListener('focus',e=>{
             color = colorpicker.value;
         })
+draw = {
+		started: false,
+		start: function(evt) {
+
+			context.beginPath();
+			context.moveTo(
+				evt.touches[0].pageX,
+				evt.touches[0].pageY
+			);
+
+			this.started = true;
+
+		},
+		move: function(evt) {
+
+			if (this.started) {
+				context.lineTo(
+					evt.touches[0].pageX,
+					evt.touches[0].pageY
+				);
+
+				context.strokeStyle = color;
+				context.lineWidth = size.value;
+				context.stroke();
+			}
+
+		},
+		end: function(evt) {
+			this.started = false;
+		}
+	};
+	
+	// Touch Events
+	myPics.addEventListener('touchstart', draw.start, false);
+	myPics.addEventListener('touchend', draw.end, false);
+	myPics.addEventListener('touchmove', draw.move, false);
+	
+	// Disable Page Move
+	document.body.addEventListener('touchmove',function(evt){
+		evt.preventDefault();
+	},false);
+};
